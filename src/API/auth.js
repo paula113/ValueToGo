4c32e1913244bcefe852614c913dd8fa6bb1b3ec
paula113 <paulapierina96@gmail.com>
2020-10-12 22:50:49 -0500

    firestore auth

--- src/API/auth.js
+++
@@ -1,7 +0,0 @@
-import firebase from '../firebase.config';
-import 'firebase/auth';
-
-export const signIn = (email, password) => firebase
-  .auth().signInWithEmailAndPassword(email, password);
-
-export const signOut = () => firebase.auth().signOut();
