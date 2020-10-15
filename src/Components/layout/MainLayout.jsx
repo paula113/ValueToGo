843127ad67ff9234c83c64b8618a8ee634c85844
almeald <almenaldana@gmail.com>
2020-10-15 08:41:09 -0500

    agrtegando container al main

--- src/Components/layout/MainLayout.jsx
+++ src/Components/layout/MainLayout.jsx
@@ -1,15 +1,16 @@
 /* eslint-disable react/prop-types */
 import React from 'react';
 import './MainLayout.scss';
+import { Container } from '@material-ui/core';
 import TopBar from '../TopBar';
 
 function MainLayout({ children }) {
   return (
-    <>
+    <main className="main-page">
       <TopBar />
-      <div>{children}</div>
+      <Container>{children}</Container>
       {/* <MobileBar /> */}
-    </>
+    </main>
   );
 }
 
