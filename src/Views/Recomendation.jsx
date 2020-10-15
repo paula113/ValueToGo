6702a4459b2c6bd18953a882fd5943e47d80d6e2
almeald <almenaldana@gmail.com>
2020-10-15 02:23:52 -0500

    creando la vista de recomendacion

---
+++ src/Views/Recomendation.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import DetailRecomendation from '../Components/DetailRecomendation';
+
+export default function Recomendation() {
+  return (
+    <div>
+      <DetailRecomendation />
+      Recomendacion container
+    </div>
+  );
+}
