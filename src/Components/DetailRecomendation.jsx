00ce2f7915c771518064e373ad9caa1fdcbe55cc
almeald <almenaldana@gmail.com>
2020-10-15 02:22:48 -0500

    creando componente para los detalles de recomendacion

---
+++ src/Components/DetailRecomendation.jsx
@@ -0,0 +1,19 @@
+import React, { useEffect } from 'react';
+// import firebase from '../firebase.config';
+
+function DetailRecomendation() {
+  return (
+    <div>
+      <h1>etalles de recomendacion</h1>
+      <h1>etalles de recomendacion</h1>
+      <h1>etalles de recomendacion</h1>
+      <h1>etalles de recomendacion</h1>
+      <h1>etalles de recomendacion</h1>
+      <h1>etalles de recomendacion</h1>
+      <h1>etalles de recomendacion</h1>
+      <h1>etalles de recomendacion</h1>
+    </div>
+  );
+}
+
+export default DetailRecomendation;
