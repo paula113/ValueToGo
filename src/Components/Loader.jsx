3ad1a0c5b5a314737edcd69844b83dba3eff5759
almeald <almenaldana@gmail.com>
2020-10-15 08:37:45 -0500

    gif de loading

--- src/Components/Loader.jsx
+++ src/Components/Loader.jsx
@@ -1,7 +1,13 @@
 import React from 'react';
+import loader from '../Assets/Loader.svg';
+import './Loader.scss';
 
 function Loader() {
-  return <div>Loading...</div>;
+  return (
+    <div className="loader">
+      <img src={loader} alt="gif loader" className="loader-image" />
+    </div>
+  );
 }
 
 export default Loader;
