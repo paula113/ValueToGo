bfb0e475f0cb5630d9f71a8b1944d0dc9a97a3cd
paula113 <paulapierina96@gmail.com>
2020-10-15 08:42:02 -0500

    new details recomendation

--- src/Components/DetailRecomendation.jsx
+++ src/Components/DetailRecomendation.jsx
@@ -1,17 +1,20 @@
 import React, { useEffect } from 'react';
+// import readXlsxFile from 'read-excel-file';
+import Details from './Details';
+import Activity from './Activity';
+import Breadcrumb from './BreadCrumb';
+// import './socio.scss';
 // import firebase from '../firebase.config';
 
 function DetailRecomendation() {
   return (
-    <div>
-      <h1>etalles de recomendacion</h1>
-      <h1>etalles de recomendacion</h1>
-      <h1>etalles de recomendacion</h1>
-      <h1>etalles de recomendacion</h1>
-      <h1>etalles de recomendacion</h1>
-      <h1>etalles de recomendacion</h1>
-      <h1>etalles de recomendacion</h1>
-      <h1>etalles de recomendacion</h1>
+    <div className="detalle-recomedaciones">
+      <Breadcrumb />
+      <p>Lee la Carta de Recomendación y responde con las siguientes acciones</p>
+      <section className="detalle-features">
+        <Details />
+        <Activity />
+      </section>
     </div>
   );
 }
