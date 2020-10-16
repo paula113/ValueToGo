d1bf2cd06d0c7b78ca6cb05e4a2e8023e7dade17
almeald <almenaldana@gmail.com>
2020-10-16 00:52:46 -0500

    un pooco de estilos a dashboard

--- src/Components/DashboardContent.jsx
+++ src/Components/DashboardContent.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import PieChart2 from './PieChart2';
 import Barchart from './Barchart';
+import './Dashboard.scss';
 
 function DashboardContent() {
   return (
-    <div>
-      <PieChart2 />
+    <div className="content">
       <Barchart />
+      <PieChart2 />
     </div>
   );
 }
