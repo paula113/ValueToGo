563b73bdbe5e4d55a03fa5802295e92cc0290844
paula113 <paulapierina96@gmail.com>
2020-10-16 00:11:00 -0500

    final styles

--- src/Components/Details.jsx
+++ src/Components/Details.jsx
@@ -9,7 +9,29 @@ export default function Details(props) {
   return (
     <div className="details-info">
       <section className="details-info-container">
-        {Object.entries(obj).map(([keys, value]) => (
+        <div className="details-info-obj">
+          <h5>Title</h5>
+          <p>{obj.title}</p>
+          <h5>Audit Unit</h5>
+          <p>{obj.auditUnit}</p>
+          <h5>Rubro</h5>
+          <p>{obj.fsli}</p>
+          <h5>Status</h5>
+          <p>{obj.status}</p>
+          <h5>Description</h5>
+          <p>{obj.description}</p>
+          <h5>Final Conclusion On Severity</h5>
+          <p>{obj.finalConclusionOnSeverity}</p>
+          <h5>Example</h5>
+          <p>{obj.example}</p>
+          <h5>Recomendation</h5>
+          <p>{obj.recomendation}</p>
+          {console.log(obj)}
+          {/* <h5>{obj.tile}</h5> example */}
+          {/* <p>{value}</p> */}
+        </div>
+
+        {/* {Object.entries(obj).map(([keys, value]) => (
           <div key={keys} className="details-info-obj">
             <h5>
               {validateStrings(keys) === 'Fsli'
@@ -18,7 +40,7 @@ export default function Details(props) {
             </h5>
             <p>{value}</p>
           </div>
-        ))}
+        ))} */}
       </section>
 
       {/* <section> */}
