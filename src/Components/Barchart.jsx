b6f07e67f4b1363c7035be4d131e082e9d346953
almeald <almenaldana@gmail.com>
2020-10-16 09:03:35 -0500

    trasladando la data a otro file

--- src/Components/Barchart.jsx
+++ src/Components/Barchart.jsx
@@ -8,37 +8,7 @@ import {
   Tooltip,
   Legend,
 } from 'recharts';
-
-const data = [
-  {
-    name: 'MON',
-    value: 1500,
-  },
-  {
-    name: 'TUE',
-    value: 2000,
-  },
-  {
-    name: 'WED',
-    value: 3000,
-  },
-  {
-    name: 'THU',
-    value: 4000,
-  },
-  {
-    name: 'FRI',
-    value: 1000,
-  },
-  {
-    name: 'SAT',
-    value: 2100,
-  },
-  {
-    name: 'SUN',
-    value: 1900,
-  },
-];
+import { dataFsli } from '../API/dataDashboard';
 
 export default class Example extends PureComponent {
   static jsfiddleUrl = 'https://jsfiddle.net/alidingling/90v76x08/';
@@ -46,9 +16,9 @@ export default class Example extends PureComponent {
   render() {
     return (
       <BarChart
-        width={500}
-        height={300}
-        data={data}
+        width={1000}
+        height={400}
+        data={dataFsli}
         margin={{
           top: 20,
           right: 30,
@@ -62,7 +32,6 @@ export default class Example extends PureComponent {
         <Tooltip />
         <Legend />
         <Bar dataKey="value" stackId="a" fill="#EB8C00" />
-        {/* <Bar dataKey="uv" stackId="a" fill="#82ca9d" /> */}
       </BarChart>
     );
   }
